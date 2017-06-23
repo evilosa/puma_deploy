a878c8a242e6ea09ca4974521eb055a24c430b79
evilosa <bojkov@sorc.ru>
2017-06-23 21:35:27 +0300

    Add puma config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "evilosa.com", user: "deployer", roles: %w{app db web}
+server "5.189.161.162", user: "deployer", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
